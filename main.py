b1ce916e251a669470a6edab12e849fbdd0dbb28
lukasz <l.szwaczka90@gmail.com>
2025-11-16 15:33:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        print('nom')
+        food.refresh()
+
+screen.exitonclick()
