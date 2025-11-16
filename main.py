32747a3ae393769aeda6cb100a5041b5b7cfb866
lukasz <l.szwaczka90@gmail.com>
2025-11-16 16:39:26 +0100

    Added scoreboard functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 #testowa zmiana
@@ -13,6 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -21,6 +23,7 @@ screen.onkey(snake.left, 'Left')
 screen.onkey(snake.right, 'Right')
 
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
@@ -31,5 +34,7 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         print('nom')
         food.refresh()
+        scoreboard.refresh()
+        print(scoreboard.score)
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,22 @@
 from turtle import Turtle
+ALIGNMENT = 'center'
+FONT = ('arial', 24, 'normal')
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.write()
-        #test drugi
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.pencolor('white')
+        self.goto(0, 260)
+
+        self.write(f'Score: {self.score}', False, align=ALIGNMENT, font=FONT)
+
+
+    def refresh(self):
+        self.clear()
+        self.score += 1
+        self.write(f'Score: {self.score}', False, align=ALIGNMENT, font=FONT)
+
+
